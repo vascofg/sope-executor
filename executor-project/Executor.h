96684b55def0e4f6c247c39c4f569e4e59cef0e5
andrefreitas <p.andrefreitas@gmail.com>
2012-04-10 01:37:04 +0100

    Added Executor struc and function headers implementation.

---
+++ executor-project/Executor.h
@@ -0,0 +1,37 @@
+#ifndef EXECUTOR_H
+#define	EXECUTOR_H
+
+/**
+ * Executor.h - A struct for describing a program that allows a user to launch, 
+ * terminate and get information about commands that he gave.
+ */
+struct Executor {
+    int runningProcesses;
+    int terminatedProcesses;
+    char* logFileName;
+    char* errorFileName;
+};
+/**
+ * Create a new executor
+ * @param e the executor pointer
+ * @param logFileName the name of the log file
+ * @param errorFileName the name of the error file
+ */
+void executor_create(struct Executor *e,char* logFileName, char* errorFileName);
+
+// Main cycle functions
+void executor_run(struct Executor *e);
+void executor_printHeader(struct Executor *e);
+char executor_receiveOrder(struct Executor *e);
+
+// User orders
+void executor_launch(struct Executor *e);
+void executor_inform(struct Executor *e);
+void executor_terminate(struct Executor *e);
+void executor_exit(struct Executor *e);
+
+// Other methods
+void executor_printlogs(struct Executor *e);
+
+#endif	/* EXECUTOR_H */
+
